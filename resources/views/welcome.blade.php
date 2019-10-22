5d42f69363d4fcbc0591fe818920ce51e64c2e1f
tahiraj07 <islam.tahiraj@hotmail.com>
2019-10-22 17:39:21 +0200

    Commit package-lock.json

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Project
                 </div>
 
                 <div class="links">
